95645f48b9d327d9d56956185167670d0304be5f
Lucas Emanoel <lucws2002@gmail.com>
2026-07-27 18:38:44 -0400

    feat: add cookie parameters for main.ts to handle authentication by cooking

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'reflect-metadata';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const options = new DocumentBuilder()
     .setTitle('Base API')
     .setDescription('Base API')
     .setVersion('1.0')
+    .addCookieAuth('sid')
     .setBasePath('api')
-    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, transform: true })
-  )
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 
