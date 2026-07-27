7049e842295892f009f911692b95330b18ee5e22
Lucas Emanoel <lucws2002@gmail.com>
2026-07-27 18:40:23 -0400

    feat: add new properties for Express type

---
+++ src/types/express.d.ts
@@ -0,0 +1,12 @@
+import { UserEntity } from '../users/entities/user.entity';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: UserEntity;
+      sessionId: string;
+    }
+  }
+}
+
+export {};
